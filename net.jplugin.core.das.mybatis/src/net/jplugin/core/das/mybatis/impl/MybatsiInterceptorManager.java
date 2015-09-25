fe15e26c1f1ce3ef10c71eacf9e644fef293115e
sunlet <liuhang.163@163.com>
2015-09-25 17:33:44 +0800

    commit

---
+++ net.jplugin.core.das.mybatis/src/net/jplugin/core/das/mybatis/impl/MybatsiInterceptorManager.java
@@ -0,0 +1,26 @@
+package net.jplugin.core.das.mybatis.impl;
+
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.Configuration;
+
+import net.jplugin.core.kernel.api.PluginEnvirement;
+
+public class MybatsiInterceptorManager {
+
+	public static MybatsiInterceptorManager instance = new MybatsiInterceptorManager();
+	static Interceptor[] inceptList = null;
+
+	public void init(){
+		 inceptList = PluginEnvirement.getInstance().getExtensionObjects(net.jplugin.core.das.mybatis.Plugin.EP_MYBATIS_INCEPT, Interceptor.class);
+	}
+	
+	private MybatsiInterceptorManager(){
+	}
+	
+	public void installPlugins(Configuration c){
+		for (Interceptor i:inceptList){
+			c.addInterceptor(i);
+		}
+	}
+
+}
