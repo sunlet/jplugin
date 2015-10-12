708c31be5136eda70642f55fe4bc6d2e8d2b9633
sunlet <liuhang.163@163.com>
2015-10-12 18:29:58 +0800

    commit

--- net.jplugin.core.config/src/net/jplugin/core/config/impl/PropertyFilter.java
+++ net.jplugin.core.config/src/net/jplugin/core/config/impl/PropertyFilter.java
@@ -1,14 +1,19 @@
 package net.jplugin.core.config.impl;
 
 import net.jplugin.common.kits.AssertKit;
+import net.jplugin.core.config.api.ConfigFactory;
 import net.jplugin.core.kernel.api.IPropertyFilter;
 
 public class PropertyFilter implements IPropertyFilter {
 
+	/**
+	 * 目前只是一个很简单的实现，支持整个字符串的匹配
+	 */
 	@Override
 	public String filte(String s) {
 		if (s!=null && s.startsWith("${") && s.endsWith("}")){
-			return s.substring(2, s.length()-1);
+			s = s.substring(2, s.length()-1);
+			return ConfigFactory.getStringConfig(s);
 		}else{
 			return s;
 		}
