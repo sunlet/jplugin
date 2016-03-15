edf9dc43002bac0806a2f659e706ec024277d22e
sunlet <liuhang.163@163.com>
2016-03-15 16:55:22 +0800

    simplify

--- net.jplugin.ext.webasic/src/net/jplugin/ext/webasic/impl/ESFHelper.java
+++ net.jplugin.ext.webasic/src/net/jplugin/ext/webasic/impl/ESFHelper.java
@@ -10,32 +10,23 @@ import net.jplugin.ext.webasic.impl.rmethod.RmethodController;
 import net.jplugin.ext.webasic.impl.rmethod.RmethodControllerSet;
 
 public class ESFHelper {
-
 	/**
-	 * 传入uri，返回uri的解析结果。结果包含服务定位路径，操作名
-	 * 如果找不到对应的服务响应该路径，则返回null
-	 * @param uri
-	 * @return
-	 */
-	public static ControllerMeta parseUri(String uri) {
-		return WebDriver.INSTANCE.parseControllerMeta(uri);
-	}
-	
-	/**
-	 * 好到对应的服务和方法名
+	 * 根据URI获取到对应的JavaBean
 	 * @param cm
 	 * @param arg
 	 * @return
 	 */
-	public ObjectAndMethod getObjectAndMethod(ControllerMeta cm, Class[] arg) {
+	public static Object getObject(String uri) {
+		ControllerMeta cm = WebDriver.INSTANCE.parseControllerMeta(uri);
+		
 		IControllerSet cs = cm.getControllerSet();
 		if (cs instanceof RestMethodControllerSet4Invoker) {
 			IServiceInvoker si = ServiceInvokerSet.instance.getServiceInvoker(cm.getServicePath());
-			return si.getObjectCallHelper().get(cm.getOperation(), arg);
+			return si.getObjectCallHelper().getObject();
 		} else if (cs instanceof RmethodControllerSet) {
 			RmethodControllerSet rcs = (RmethodControllerSet) cm.getControllerSet();
 			RmethodController rc = rcs.getRMethodController(cm.getServicePath());
-			return rc.getObjectCallHelper().get(cm.getOperation(), arg);
+			return rc.getObjectCallHelper().getObject();
 		} else
 			return null;
 	}
