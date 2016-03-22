1dd52d3fa4caf4631a992572924eb9d7d6fb50ce
sunlet <liuhang.163@163.com>
2016-03-22 21:59:24 +0800

    fixbug null dir

--- net.jplugin.core.log/src/net/jplugin/core/log/LogServiceImpl.java
+++ net.jplugin.core.log/src/net/jplugin/core/log/LogServiceImpl.java
@@ -28,7 +28,7 @@ public class LogServiceImpl implements ILogService {
 
 	private void init(){
 		Properties prop = PropertiesKit.loadProperties(PluginEnvirement.getInstance().getConfigDir()+"/log4j.properties");
-		PropertiesKit.replaceVar(prop, PluginEnvirement.WORK_DIR, System.getProperty(PluginEnvirement.getInstance().getWorkDir()));
+		PropertiesKit.replaceVar(prop, PluginEnvirement.WORK_DIR, PluginEnvirement.getInstance().getWorkDir());
 		PropertyConfigurator.configure(prop);
 	}
 	
