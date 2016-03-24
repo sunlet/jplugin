6ec85993ec258810e29d5a0a25f47a3f400c1e6e
sunlet <liuhang.163@163.com>
2016-03-24 15:37:36 +0800

    ignore plugin

--- net.jplugin.core.kernel/src/net/jplugin/core/kernel/api/ExtPlugin.java
+++ net.jplugin.core.kernel/src/net/jplugin/core/kernel/api/ExtPlugin.java
@@ -8,16 +8,29 @@ import net.jplugin.common.kits.ReflactKit;
 
 public class ExtPlugin {
 
-	public static Collection<? extends Object> get() {
-		Set<Object> ret = new HashSet();
+	public static Collection<String> get() {
+		Set<String> ret = new HashSet();
 		for (int i=0;i<6;i++){
 			Set<String> s = getSet(i);
-			if (s!=null)
-				ret.addAll(s);
+			if (s!=null){
+				for (String pname:s){
+					addIfExists(ret,pname);
+				}
+			}
 		}
 		return ret;
 	}
-
+	
+	private static void addIfExists(Set<String> ret,String string) {
+		try{
+			Class.forName(string);
+			ret.add(string);
+		}catch(Exception e){
+			System.out.println("IgnorePlugin : "+string+",Class not found.");
+		}
+		
+	}
+	
 	private static Set<String> getSet(int i) {
 		String cName = "net.jplugin.PluginProvider_"+i;
 		try{
