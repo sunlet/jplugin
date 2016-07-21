091793d876b0c544be3ee63eb228387e4bc76035
sunlet <liuhang.163@163.com>
2016-07-21 14:35:18 +0800

    Cookies toString

--- net.jplugin.core.kernel/src/net/jplugin/core/kernel/api/ctx/Cookies.java
+++ net.jplugin.core.kernel/src/net/jplugin/core/kernel/api/ctx/Cookies.java
@@ -2,6 +2,7 @@ package net.jplugin.core.kernel.api.ctx;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class Cookies {
@@ -18,5 +19,15 @@ public class Cookies {
 	}
 	public void _setFromMap(HashMap<String,String> m){
 		this.map = m;
+	}
+	
+	public String toString(){
+		if (map==null) return "{}";
+		StringBuffer sb  = new StringBuffer();
+		for (Entry<String, String> en:map.entrySet()){
+			sb.append(en.getKey()+"="+en.getValue());
+			sb.append(",");
+		}
+		return sb.toString();
 	}
 }
