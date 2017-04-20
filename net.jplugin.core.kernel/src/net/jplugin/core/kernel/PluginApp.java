fdc792c2764426fd798d715c4074a3edda8c2d66
sunlet <liuhang.163@163.com>
2017-04-20 11:34:45 +0800

    fixbug

--- net.jplugin.core.kernel/src/net/jplugin/core/kernel/PluginApp.java
+++ net.jplugin.core.kernel/src/net/jplugin/core/kernel/PluginApp.java
@@ -1,9 +1,24 @@
 package net.jplugin.core.kernel;
 
+import net.jplugin.common.kits.StringKit;
 import net.jplugin.core.kernel.api.PluginEnvirement;
 
 public class PluginApp {
 	public static void main(String[] args) {
+		
+		String cfgDir = System.getProperty("plugin-config.path");
+		String workDir = System.getProperty("work-dir");
+		
+		if (StringKit.isNull(cfgDir)){
+			System.out.println("Can't find [plugin-config.path] system property. use default value");
+			cfgDir = "./config";
+		}
+		PluginEnvirement.getInstance().setConfigDir(cfgDir);
+		if (StringKit.isNull(workDir)){
+			System.out.println("Can't find the [work-dir] system property. use default value");
+			workDir = "./work";
+		}
+		PluginEnvirement.getInstance().setWorkDir(workDir);
 		PluginEnvirement.getInstance().startup();
 	}
 }
