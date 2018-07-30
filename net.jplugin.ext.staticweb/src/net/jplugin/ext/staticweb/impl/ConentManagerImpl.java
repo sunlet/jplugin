c5b7abe42b579f63d49861f3c1aa7e4deca246d2
sunlet <liuhang.163@163.com>
2018-07-30 19:10:32 +0800

    增加uri的路径安全检查

--- net.jplugin.ext.staticweb/src/net/jplugin/ext/staticweb/impl/ConentManagerImpl.java
+++ net.jplugin.ext.staticweb/src/net/jplugin/ext/staticweb/impl/ConentManagerImpl.java
@@ -3,7 +3,9 @@ package net.jplugin.ext.staticweb.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.jplugin.common.kits.AssertKit;
 import net.jplugin.common.kits.FileKit;
+import net.jplugin.common.kits.StringKit;
 import net.jplugin.core.kernel.api.PluginEnvirement;
 import net.jplugin.ext.staticweb.api.IContentManager;
 
@@ -71,6 +73,11 @@ public class ConentManagerImpl implements IContentManager {
 
 	private byte[] loadContent(Request req) {
 		String uri = req.getUri();
+		
+		if (!checkPath(uri)){
+			return ("Illegal path,file="+uri).getBytes();
+		}
+		
 		String path = PluginEnvirement.INSTANCE.getWebRootPath()+uri;
 //		String path = "."+uri;
 		if (FileKit.existsAndIsFile(path)){
@@ -81,7 +88,67 @@ public class ConentManagerImpl implements IContentManager {
 		}
 	}
 	
+	/**
+	 * 因为是后加的，在不违反安全的情况下，尽量兼容
+	 * @param uri
+	 * @return
+	 */
+	private static boolean checkPath(String uri) {
+		//空，通过
+		if (StringKit.isNull(uri)) {
+			return true;
+		}
+		//开始符不对
+		if (! (uri.startsWith("/") || uri.startsWith("\\"))){
+			return false;
+		}
+		String temp = StringKit.replaceStr(uri, "\\", "/");
+		//如果只是一个/，也通过
+		if (temp.equals("/")) {
+			return true;
+		}
+		
+		//判断路径
+		String[] arr = StringKit.splitStr(temp, "/");
+		int v = 0;
+		for (String a:arr){
+			if (StringKit.isNull(a))
+				continue;
+			a = a.trim();
+			if (".".equals(a))
+				continue;
+			if ("..".equals(a))
+				v--;
+			else
+				v++;
+			
+			if (v<0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
+		AssertKit.assertEqual(checkPath(null) , true );
+		AssertKit.assertEqual(checkPath("") , true );
+		AssertKit.assertEqual(checkPath("/") , true );
+		AssertKit.assertEqual(checkPath("\\") , true );
+		AssertKit.assertEqual(checkPath("/a") , true );
+		AssertKit.assertEqual(checkPath("/././a") , true );
+		AssertKit.assertEqual(checkPath("/././.") , true );
+		AssertKit.assertEqual(checkPath("a") , false);
+		AssertKit.assertEqual(checkPath("/../a") , false);
+		AssertKit.assertEqual(checkPath("/../.") , false);
+		AssertKit.assertEqual(checkPath("/a/..") , true);
+		AssertKit.assertEqual(checkPath("/a/../..") , false);
+		AssertKit.assertEqual(checkPath("/a/b/../../.") , true);
+		AssertKit.assertEqual(checkPath("/a/b/../../a/.././..") , false);
+		AssertKit.assertEqual(checkPath("/a/b/../../a/.././..") , false);
+		AssertKit.assertEqual(checkPath("/a/b/../ ../a/.././..") , false);
+	}
+	
+	public static void main2(String[] args) {
 		
 		ConentManagerImpl o = new ConentManagerImpl();
 		Response res = o.handleRequest(Request.create("/src/net/jplugin/ext/staticweb/Plugin.java"));
