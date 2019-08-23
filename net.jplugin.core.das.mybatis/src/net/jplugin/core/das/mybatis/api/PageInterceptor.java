228e7a2823c8a75895110ac42191abe5ba98ad3d
peiyu <125331682@qq.com>
2019-08-23 11:09:30 +0800

    优化分页时count语句性能

--- net.jplugin.core.das.mybatis/src/net/jplugin/core/das/mybatis/api/PageInterceptor.java
+++ net.jplugin.core.das.mybatis/src/net/jplugin/core/das/mybatis/api/PageInterceptor.java
@@ -115,7 +115,8 @@ public abstract class PageInterceptor implements Interceptor {
     private void setPageParameter(String sql, Connection connection, MappedStatement mappedStatement,
             BoundSql boundSql, PageCond page) throws SQLException {
         // 记录总记录数
-        String countSql = "select count(0) from (" + sql + ")  total";
+    	CountSqlParser parser = new CountSqlParser();
+        String countSql = parser.getSmartCountSql(sql);
         PreparedStatement countStmt = null;
         ResultSet rs = null;
         try {
