6e0493d83cb3a3a1c4f3b45464ea706fcbb78233
sunlet <liuhang.163@163.com>
2019-09-19 13:41:04 +0800

    修改Hash算法，在IN 条件时候不再查询所有表

--- net.jplugin.core.das/ts-src/net/jplugin/core/das/route/impl/algms/HashAlgm.java
+++ net.jplugin.core.das/ts-src/net/jplugin/core/das/route/impl/algms/HashAlgm.java
@@ -1,16 +1,21 @@
 package net.jplugin.core.das.route.impl.algms;
 
-import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
-import net.jplugin.common.kits.CalenderKit;
 import net.jplugin.core.das.route.api.DataSourceInfo;
 import net.jplugin.core.das.route.api.ITsAlgorithm;
 import net.jplugin.core.das.route.api.RouterDataSource;
 import net.jplugin.core.das.route.api.RouterDataSourceConfig.DataSourceConfig;
 import net.jplugin.core.das.route.api.RouterDataSourceConfig.TableConfig;
-import net.jplugin.core.das.route.api.TablesplitException;
-import net.jplugin.core.das.route.api.ITsAlgorithm.ValueType;
 import net.jplugin.core.das.route.api.RouterKeyFilter;
+import net.jplugin.core.das.route.api.RouterKeyFilter.Operator;
+import net.jplugin.core.das.route.api.TablesplitException;
 
 public class HashAlgm  implements ITsAlgorithm{
 
@@ -45,10 +50,44 @@ public class HashAlgm  implements ITsAlgorithm{
 	}
 	
 	/**
-	 * 目前都是返回所有的表，其实对于IN类型可以做一些优化的。
+	 * 
 	 */
 	@Override
 	public DataSourceInfo[] getMultiResults(RouterDataSource compondDataSource, String tableBaseName,ValueType valueType ,RouterKeyFilter kva) {
+		if (Operator.EQUAL.equals(kva.getOperator())){
+			Result r = getResult(compondDataSource, tableBaseName, valueType, kva.getConstValue()[0]);
+			DataSourceInfo[] ret = new DataSourceInfo[1];
+			ret[0] =  DataSourceInfo.build(r.getDataSource(), new String[]{r.getTableName()});
+			return ret;
+		}
+		if (Operator.IN.equals(kva.getOperator())){
+			//获取去重的结果集合
+			Map<String,Set<String>> resultsMap = new HashMap();
+			for (int i=0;i<kva.getConstValue().length;i++){
+				Result r = getResult(compondDataSource, tableBaseName, valueType, kva.getConstValue()[i]);
+				Set<String> targetList = resultsMap.get(r.getDataSource());
+				if (targetList==null){
+					targetList = new HashSet();
+					resultsMap.put(r.getDataSource(), targetList);
+				}
+				if (!targetList.contains(r.getTableName())){
+					targetList.add(r.getTableName());
+				}
+			}
+			//转换成结果的格式
+			DataSourceInfo[] ret = new DataSourceInfo[resultsMap.size()];
+			int i=0;
+			for (Entry<String, Set<String>> en:resultsMap.entrySet()){
+				String[] tbs = new String[en.getValue().size()];
+				ret[i++] = DataSourceInfo.build(en.getKey(), en.getValue().toArray(tbs));
+			}
+			return ret;
+		}else{
+			return getAllTables(compondDataSource, tableBaseName);
+		}
+	}
+
+	private DataSourceInfo[] getAllTables(RouterDataSource compondDataSource, String tableBaseName) {
 		TableConfig tableCfg = compondDataSource.getConfig().findTableConfig(tableBaseName);
 		int splits = tableCfg.getSplits();
 		DataSourceConfig[] dscfg = compondDataSource.getConfig().getDataSourceConfig();
