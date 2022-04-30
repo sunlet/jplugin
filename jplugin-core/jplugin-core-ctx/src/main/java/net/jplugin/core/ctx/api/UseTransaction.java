cbac98e0c1bcb9cf520eb13c8ea7835e3aa14efd
sunlet <liuhang.163@163.com>
2022-04-30 13:37:29 +0800

    add comment

--- jplugin-core/jplugin-core-ctx/src/main/java/net/jplugin/core/ctx/api/UseTransaction.java
+++ jplugin-core/jplugin-core-ctx/src/main/java/net/jplugin/core/ctx/api/UseTransaction.java
@@ -4,8 +4,10 @@ import java.lang.annotation.*;
 
 /**
  * <PRE>
- * 此标注用在BindService注册的扩展类的方法上，实现对这个方法的事务控制。
+ * 此标注用在service扩展点（BindService注册的扩展类）的方法上，实现对这个方法的事务控制。
  * 如果事务控制的类在执行过程中又调用了事务操作，则内层事务无效，合并到外层事务当中。
+ *
+ * 注意：只可以标注在public/protected/default类型方法上，标注在private类型方法上无法产生事务
  * </PRE>
  */
 
