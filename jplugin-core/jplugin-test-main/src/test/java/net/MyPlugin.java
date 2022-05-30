2b2204b7b35c1f0f0c9c234da903d5e4a28a039a
sunlet <liuhang.163@163.com>
2022-05-30 20:50:27 +0800

    modify

---
+++ jplugin-core/jplugin-test-main/src/test/java/net/MyPlugin.java
@@ -0,0 +1,15 @@
+package net.luis;
+
+import net.jplugin.core.kernel.api.AbstractPlugin;
+
+public class MyPlugin extends AbstractPlugin {
+    @Override
+    public int getPrivority() {
+        return 0;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
