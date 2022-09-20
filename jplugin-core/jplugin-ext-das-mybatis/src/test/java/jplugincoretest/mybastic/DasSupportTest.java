4aed37893c6bd2742a76b71bd26a047bae301a9b
sunlet <liuhang.163@163.com>
2022-09-20 11:40:02 +0800

    upgrade jackson xml; add Dassupport

--- jplugin-core/jplugin-ext-das-mybatis/src/test/java/jplugincoretest/mybastic/DasSupportTest.java
+++ jplugin-core/jplugin-ext-das-mybatis/src/test/java/jplugincoretest/mybastic/DasSupportTest.java
@@ -1,2 +1,28 @@
-package jplugincoretest.mybastic;public class DasSupportTest {
+package jplugincoretest.mybastic;
+
+import net.jplugin.common.kits.AssertKit;
+import net.jplugin.core.das.mybatis.api.SqlSessionDaoSupport;
+import net.jplugin.core.kernel.api.Initializable;
+import net.jplugin.core.service.api.BindService;
+import net.jplugin.core.service.api.RefService;
+
+@BindService
+public class DasSupportTest extends SqlSessionDaoSupport implements Initializable {
+
+    @Override
+    public String getDataSourceName() {
+        return "database";
+    }
+
+    public void test(){
+        AssertKit.assertTrue(getSqlSession()!=null);
+
+    }
+
+    @RefService
+    DasSupportTest self;
+    @Override
+    public void initialize() {
+        self.test();
+    }
 }
