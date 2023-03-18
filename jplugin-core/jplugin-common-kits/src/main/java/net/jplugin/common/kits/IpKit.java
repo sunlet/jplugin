01839713c7864cb68a959435ff16d4bbc04f129e
sunlet <liuhang.163@163.com>
2023-03-18 19:21:33 +0800

    add getLocalIp

--- jplugin-core/jplugin-common-kits/src/main/java/net/jplugin/common/kits/IpKit.java
+++ jplugin-core/jplugin-common-kits/src/main/java/net/jplugin/common/kits/IpKit.java
@@ -1,5 +1,9 @@
 package net.jplugin.common.kits;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,6 +18,31 @@ public class IpKit {
 	     Matcher matcher = p.matcher(ip);
 	     boolean result = matcher.find();
 	     return !result;
+	}
+
+	public static String getLocalIp() {
+		StringBuilder ip = new StringBuilder();
+
+		try {
+			Enumeration en = NetworkInterface.getNetworkInterfaces();
+
+			while(en.hasMoreElements()) {
+				NetworkInterface intf = (NetworkInterface)en.nextElement();
+				Enumeration enumIpAddr = intf.getInetAddresses();
+
+				while(enumIpAddr.hasMoreElements()) {
+					InetAddress inetAddress = (InetAddress)enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress() && inetAddress.isSiteLocalAddress() && intf.getDisplayName().indexOf("Virtual") < 0) {
+						ip.append(inetAddress.getHostAddress() + ",");
+					}
+				}
+			}
+		} catch (SocketException var5) {
+			System.out.println("kmonitor get ip exception: " + var5.getMessage());
+		}
+
+		String gotIp = ip.toString();
+		return gotIp.length() > 0 ? gotIp.substring(0, gotIp.lastIndexOf(",")) : null;
 	}
 	
 	public static void main(String[] args) {
